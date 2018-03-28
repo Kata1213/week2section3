a9633ebed7c97031c2757047b619fedccb270ee1
Kata <2726456039@qq.com>
2018-03-28 21:28:53 +0800

    modified:   src/main/java/cn/school/thoughtworks/section1/PracticeB.java 	modified:   src/main/java/cn/school/thoughtworks/section1/PracticeC.java

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -7,8 +7,7 @@ import java.util.List;
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-            //实现练习要求，并改写该行代码。
+
             if(collection1.size()==0||collection2.size()==0){
                 return null;
             }
--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,33 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        if(collection1.size()==0||collection2.size()==0){
+            return null;
+        }
+
+        List<String> list=new ArrayList<String>() ;
+        LinkedHashMap<String,Integer> map =new LinkedHashMap<>();
+
+
+
+        for (List<String> strings : collection2.values()) {
+            for (String string : strings) {
+                map.put(string,1);
+            }
+        }
+        for (String s : collection1) {
+            if (map.containsKey(s)) {
+                list.add(s);
+            }
+        }
+
+        return  list;
     }
 }
